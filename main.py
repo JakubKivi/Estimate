dab8333bb42a4b74646527c70399dbe2f556d641
jakubkivi <panmcleon@gmail.com>
2020-05-02 19:50:10 +0200

    text is on the screen

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ import random
 import math
 from pygame.locals import *
 
+buffor = random.randint(1, 100)
+number = math.sqrt(buffor)
+counter = 1
+n1 = "_"
+n2 = "_"
+n3 = "_"
+n4 = "_"
+n5 = "_"
+n6 = "_"
+actualText = n1+" "+n2+" "+n3+"  <  sqrt(" + str(buffor) + ")  <  "+n1+" "+n2+" "+n3
+k = None
 
 class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 940, 800
+        self.size = self.width, self.height = 940, 800
 
     def on_init(self):
         pygame.init()
@@ -20,28 +31,28 @@ class App:
         if event.type == pygame.QUIT:
             self._running = False
         if event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_0:
-                k = 0
-            if event.key == pygame.K_1:
-                k = 1
-            if event.key == pygame.K_2:
-                k = 2
-            if event.key == pygame.K_3:
-                k = 3
-            if event.key == pygame.K_4:
-                k = 4
-            if event.key == pygame.K_5:
-                k = 5
-            if event.key == pygame.K_6:
-                k = 6
-            if event.key == pygame.K_7:
-                k = 7
-            if event.key == pygame.K_8:
-                k = 8
-            if event.key == pygame.K_9:
-                k = 9
-
+            if event.key == pygame.K_0 or event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4 or event.key == pygame.K_5 or event.key == pygame.K_6 or event.key == pygame.K_7 or event.key == pygame.K_8 or2 event.key == pygame.K_9:
+                if event.key == pygame.K_0:
+                    k = 0
+                if event.key == pygame.K_1:
+                    k = 1
+                if event.key == pygame.K_2:
+                    k = 2
+                if event.key == pygame.K_3:
+                    k = 3
+                if event.key == pygame.K_4:
+                    k = 4
+                if event.key == pygame.K_5:
+                    k = 5
+                if event.key == pygame.K_6:
+                    k = 6
+                if event.key == pygame.K_7:
+                    k = 7
+                if event.key == pygame.K_8:
+                    k = 8
+                if event.key == pygame.K_9:
+                    k = 9
+            counter += 1
     def on_loop(self):
         pass
 
@@ -60,15 +71,17 @@ class App:
             for event in pygame.event.get():
                 self.on_event(event)
 
-            buffor = random.randint(1, 100)
+            font = pygame.font.SysFont("Arial", 72)
+            text = font.render(actualText, True, (0, 128, 0))
 
-            number = math.sqrt(buffor)
-            myfont = pygame.font.SysFont("monospace", 15)
-            label = myfont.render("Some text!", 1, (255, 255, 0))
-            screen.blit(label, (100, 100))
+            self._display_surf.fill((255, 255, 255))
+            self._display_surf.blit(text,
+                        ((self.width - text.get_width())/2, (self.height - text.get_height())/2))
 
-            print(buffor)
-            print(number)
+            pygame.display.flip()
+
+            # print(buffor)
+            # print(number)
 
             if k < 10:
                 k = 10
@@ -80,5 +93,8 @@ class App:
 
 
 if __name__ == "__main__":
+
     theApp = App()
     theApp.on_execute()
+
+
